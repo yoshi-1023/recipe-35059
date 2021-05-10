b19f0b3edaea41e23895c71a207f4c522963b773
yoshi-1023 <yoshika.122355@gmail.com>
2021-05-10 13:59:47 +0900

    cocoon導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("@nathanvda/cocoon")
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
+  accepts_nested_attributes_for :ingredients, allow_destroy: true, reject_if: :all_blank
 end
